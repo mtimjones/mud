03431550c11fe2c997e19d7a0c797ebe2a767aa7
mtimjones <mtj@mtjones.com>
2024-10-02 19:35:00 -0700

    Begin new player logic.

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 from world import World
+from threading import Thread, Lock
 
 class Player:
 
@@ -10,11 +11,13 @@ class Player:
         self.username = None
         self.password = None
         self.json = self.world.get()
+        self.send_mutex = Lock()
 
 
     def send_response(self, message):
 
-        self.client_socket.send(message.encode())
+        with self.send_mutex:
+            self.client_socket.send(message.encode())
 
 
     def interact(self, message) -> None:
@@ -27,21 +30,24 @@ class Player:
         if self.state in range(0,10):
             self.login(message)
 
-        elif self.state in range(10,20):
+        elif self.state in range(10, 20):
+            self.newchar(message)
+
+        elif self.state in range(20,30):
             self.play(message)
 
 
     def login(self, message):
 
         if self.state == 0:
-            response = "Enter 'new' to create a new character, or your username.\n\r"
-            self.send_response(response)
+            self.send_response("Enter 'new' to create a new character, or your username.\n\r")
             self.state = 1
 
         elif self.state == 1:
             if message == "new":
-                pass
                 #Create a new character
+                self.send_response("Enter your character's name: \n\r")
+                self.state = 10
             else:
                 found = False
                 for users in self.json['entities']:
@@ -52,10 +58,9 @@ class Player:
                         self.state = 9
 
                 if not found:
-                    response = f"""User {message} not found.\n\r
-                                Enter your username or 'new' to create a new character.\n\r"""
+                    response = f"User {message} not found.\n\r Enter your username or 'new' to create a new character.\n\r"
                     self.send_response(response)
-                    self.state = 1
+                    self.state = 0
 
         elif self.state == 9:
             found = False
@@ -64,20 +69,30 @@ class Player:
                     found = True
                     self.password = message
                     self.send_response("You are now logged in.\n\r")
-                    self.state = 10
+                    self.state = 20
                     self.play("")
 
             if not found:
-                response = f"""Password failed.\n\r
-                            Enter your username or 'new' to create a new character.\n\r"""
+                response = f"Password failed.\n\rEnter your username or 'new' to create a new character.\n\r"
                 self.send_response(response)
-                self.state = 1
+                self.state = 0
+
+
+    def newchar(self, message):
+
+        self.send_response("We're here for new char.\n\r")
+
+        # Make sure that the player doens't exist.
+
+        # If so, request the password.
+
+        # Otherwise, return to self.state = 0
+
+        pass
 
 
     def play(self, message):
 
-        self.send_response("Reached play\n")
-
-
+        self.send_response("Reached play\n\r")
 
 
